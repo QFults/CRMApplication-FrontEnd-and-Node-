6ac46abc3eb4e3f7056018b2b1eabcb7338a7cfd
Quinton Fults <quinton.fults.work@gmail.com>
2016-12-08 10:03:42 -0800

    one small change

--- config/routes.js
+++ config/routes.js
@@ -11,11 +11,13 @@ var emailController = require('../controllers/emailControllerNode');
 
 // what the route looks like between your public and node controllers
 // just examples, will have more routes based on different methods in controllers
-router.get('/user', userController.test);
-router.get('/customer', customerController.test);
-router.get('/note', noteController.test);
-router.get('/sms', smsController.test);
-router.get('/email', emailController.test);
+
+// 
+router.get('/user', userController);
+router.get('/customer', customerController);
+router.get('/note', noteController);
+router.get('/sms', smsController);
+router.get('/email', emailController);
 router.get('/customer/newInfo', customerController.getCustomerInfo);
 
 module.exports = router;
