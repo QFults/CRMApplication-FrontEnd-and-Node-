bbf699224b39fa0c5beaf424b8cca412e094152b
Neil Mosunic <neilm813@gmail.com>
2016-12-13 09:28:19 -0800

    updated userService

--- public/services/userService.js
+++ public/services/userService.js
@@ -2,10 +2,13 @@ angular.module("CRMApp").service("userService", function ($http) {
 
   this.test = 'service works!';
   this.users = [];
-
+  this.seletedUser = {};
   this.index = function (allUsers) {
     this.users = allUsers;
   }
 
+  this.setSelectedUser = function (user) {
+    this.selectedUser = user;
+  }
+  
 });
-
