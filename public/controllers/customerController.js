9c73c59eded2a468117895484609e3752ea51864
Neil Mosunic <neilm813@gmail.com>
2016-12-13 18:03:32 -0800

    full contact works on customer.html

--- public/controllers/customerController.js
+++ public/controllers/customerController.js
@@ -1,10 +1,10 @@
 angular.module("CRMApp").controller("customerController", function ($scope, $http, customerService, userService, noteService) {
 
-    $scope.customerEmail = '';
+    $scope.customerEmail = 'rhythmic88@gmail.com';
     $scope.customerPhone = '';
     $scope.customerInfo = '';
-    $scope.emailCheckResult = '';
-    $scope.phoneCheckResult = '';
+    $scope.fcEmailResults = ''; // set to seed data at bottom
+    $scope.fcPhoneResults = '';
     $scope.filtering = '';
     $scope.displayedCustomers = [];
     $scope.selectedUser = {};
@@ -30,19 +30,20 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
             })
     }
 
-    $scope.searchCustomerEmail = function () {
-        $scope.emailCheckResult = 'loading';
+    $scope.getFCByEmail = function () {
+        $scope.fcEmailResults = 'loading';
         $http.get(`http://localhost:3000/customers/byEmail?email=${$scope.customerEmail}`)
             .then(function (response) {
-                $scope.emailCheckResult = response.data.object;
+                $scope.fcEmailResults = response.data.object;
+                console.log(response.data.object);
             })
     }
 
     $scope.searchCustomerPhone = function () {
-        $scope.phoneCheckResult = 'loading';
+        $scope.fcPhoneResults = 'loading';
         $http.get(`http://localhost:3000/customers/byPhone?phone=${$scope.customerPhone}`)
             .then(function (response) {
-                $scope.phoneCheckResult = response.data.object;
+                $scope.fcPhoneResults = response.data.object;
             })
     }
 
@@ -57,6 +58,8 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
         noteService.getNotes(response.data.notes);
     })
 }
+
+    $scope.fcEmailResults = {"status":200,"requestId":"479f8373-d50b-486a-acbd-cd538421946e","likelihood":0.88,"photos":[{"type":"facebook","typeId":"facebook","typeName":"Facebook","url":"https://d2ojpxxtu63wzl.cloudfront.net/static/6d3570d1dd1347b56f491794e0924427_aa9d0e448fa5984ac52dee8d501edf61b271e2661de16b0f65e7b8441dea7847","isPrimary":true},{"type":"linkedin","typeId":"linkedin","typeName":"LinkedIn","url":"https://d2ojpxxtu63wzl.cloudfront.net/static/d87d9548cca163016e66d9bd2021e009_603c580ad7e4028483f349edf054e54d389590d48330453a41f4011f498bf83d"},{"type":"gravatar","typeId":"gravatar","typeName":"Gravatar","url":"https://d2ojpxxtu63wzl.cloudfront.net/static/a3fbc2a91e003309dd5618b6ed7fea98_c594e9af3addf59ba41a907ba5784ba8ee23fb37089f1e1dbbba039d3cff05ff"},{"type":"angellist","typeId":"angellist","typeName":"Facebook","url":"https://d2ojpxxtu63wzl.cloudfront.net/static/dd7714430e06e43780b6b573c9805795_b1af65e8490498640027007c414caf88b4668d270fad927d9c74a798948c7bca"}],"contactInfo":{"websites":[{"url":"http://teamtreehouse.com/kylepinzon"}],"familyName":"Pinzon","fullName":"Kyle Pinzon","givenName":"Kyle"},"organizations":[{"name":"Digsy","startDate":"2012-12-31","title":"Customer Success Manager","current":true},{"name":"BrokerRoster","startDate":"2012-06-30","title":"Community Manager and Growth Hacker","current":true},{"name":"Digsy","startDate":"2012-06","title":"Director of Client Success","current":true},{"name":"Integrated Resources Institute","startDate":"2012-06","endDate":"2013-08","title":"Job Coach"},{"name":"JP Morgan Chase Bank","startDate":"2007-12","endDate":"2012"}],"demographics":{"locationDeduced":{"normalizedLocation":"La Habra, California","deducedLocation":"La Habra, California, United States","city":{"name":"La Habra"},"state":{"name":"California","code":"CA"},"country":{"deduced":true,"name":"United States","code":"US"},"continent":{"deduced":true,"name":"North America"},"county":{"deduced":true,"name":"Orange"},"likelihood":1},"gender":"Male","locationGeneral":"La Habra, California"},"socialProfiles":[{"followers":9,"type":"angellist","typeId":"angellist","typeName":"AngelList","url":"https://angel.co/kyle-pinzon","username":"kyle-pinzon","id":"803582"},{"type":"facebook","typeId":"facebook","typeName":"Facebook","url":"https://www.facebook.com/kyle.pinzon"},{"type":"flickr","typeId":"flickr","typeName":"Flickr","url":"https://www.flickr.com/people/69246681@N07","username":"kylepinzon","id":"69246681@N07"},{"type":"foursquare","typeId":"foursquare","typeName":"Foursquare","url":"https://foursquare.com/user/3073730","id":"3073730"},{"type":"github","typeId":"github","typeName":"Github","url":"https://github.com/kpinzon","username":"kpinzon"},{"type":"google","typeId":"google","typeName":"GooglePlus","url":"https://plus.google.com/107747333110667626493","id":"107747333110667626493"},{"type":"gravatar","typeId":"gravatar","typeName":"Gravatar","url":"https://gravatar.com/kylepinzon","username":"kylepinzon","id":"49801145"},{"followers":212,"following":212,"type":"linkedin","typeId":"linkedin","typeName":"LinkedIn","url":"https://www.linkedin.com/in/kylepinzon","username":"kylepinzon"}]};
 
 });
 
