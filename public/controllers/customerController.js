8d839867473ad772533e6f54dd11112bd9373501
Neil Mosunic <neilm813@gmail.com>
2016-12-20 17:56:54 -0800

    select mood styling finished

--- public/controllers/customerController.js
+++ public/controllers/customerController.js
@@ -25,8 +25,6 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
     $scope.customerId = '';
     $scope.selectedCustomer = customerService.getSelectedCustomer();
     $scope.editedCustomer = {};
-    $scope.fcByEmailShown = false;
-    $scope.fcByPhoneShown = false;
     // Users
     $scope.selectedUserName = 'Select A User';
     $scope.loggedInUser = userService.getLoggedInUser();
@@ -43,7 +41,8 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
     // Full Contact
     $scope.fcEmailResults = '';
     $scope.fcPhoneResults = '';
-
+    $scope.fcByEmailShown = false;
+    $scope.fcByPhoneShown = false;
     $scope.check = function () {
         console.log(userService.getLoggedInUser())
     }
@@ -254,8 +253,28 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
     /* NOTES
     **************************************/
 
-    $scope.setNewNoteMood = function (mood) {
-        $scope.newNoteMood = mood;
+    $scope.setNoteMood = function (mood, status) {
+        if ( status == 'new' ) {
+            $scope.newNoteMood = mood;
+        }
+        if ( status == 'edit' ) {
+            $scope.editedNote.Mood = mood;
+        }
+
+        $scope.isHappy = false;
+        $scope.isNeutral = false;
+        $scope.isMad = false;
+        $('.moodDivs').removeClass('botBordBlue');
+        $('.moodBtn').blur();
+        if ( mood == 'Happy' ) {
+            $scope.isHappy = true;
+        } 
+        else if ( mood == 'Neutral' ) {
+            $scope.isNeutral = true;
+        }
+        else {
+            $scope.isMad = true;
+        }
     }
 
     $scope.saveNewNote = function () {
@@ -263,7 +282,8 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
             alert("Please specify a mood, subject, and body before saving.");
         }
         else {
-
+            $('.moodDivs').removeClass('botBordBlue');
+            $('.moodBtn').blur();
             var dateAdded = returnDate();
             var authorName = $scope.loggedInUser.FirstName + ' ' + $scope.loggedInUser.LastName;
             $http.post('http://localhost:3000/notes/',
@@ -301,6 +321,8 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
     $scope.editNote = function (note) {
         $scope.editedNote = angular.copy(note);
         $("#editNoteModal").modal();
+        $('.moodDivs').removeClass('botBordBlue');
+        $('.moodBtn').blur();
     }
 
     $scope.saveEditedNote = function () {
@@ -308,6 +330,8 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
             alert( 'Please fill out both the subject line and body of before saving.' );
         }
         else {
+            $('.moodDivs').removeClass('botBordBlue');
+            $('.moodBtn').blur();
             $http.put('http://localhost:3000/notes/' + $scope.editedNote.Id,$scope.editedNote
             )
             .then(function (response) {
@@ -341,22 +365,28 @@ angular.module("CRMApp").controller("customerController", function ($scope, $htt
     **************************************/
 
     $scope.getFCByEmail = function () {
-        $scope.fcEmailResults = 'loading';
+        console.log($scope.selectedCustomer.Email);
+        $scope.fcByEmailShown = true
+        $scope.fcEmailStatus = 'searching...';
         $http.get(`http://localhost:3000/customers/byEmail?email=${$scope.selectedCustomer.Email}`)
             .then(function (response) {
+                $scope.fcEmailStatus = null;
                 $scope.fcEmailResults = response.data.object;
+                console.log(response.data.object);
             });
     }
 
     $scope.searchCustomerPhone = function () {
-        $scope.fcPhoneResults = 'loading';
+        $scope.fcPhoneResults = 'searching...';
         $http.get(`http://localhost:3000/customers/byPhone?phone=${$scope.selectedCustomer.Phone}`)
             .then(function (response) {
                 $scope.fcPhoneResults = response.data.object;
             });
     }
 
-    /*$scope.fcEmailResults = { "status": 200, "requestId": "479f8373-d50b-486a-acbd-cd538421946e", "likelihood": 0.88, "photos": [{ "type": "facebook", "typeId": "facebook", "typeName": "Facebook", "url": "https://d2ojpxxtu63wzl.cloudfront.net/static/6d3570d1dd1347b56f491794e0924427_aa9d0e448fa5984ac52dee8d501edf61b271e2661de16b0f65e7b8441dea7847", "isPrimary": true }, { "type": "linkedin", "typeId": "linkedin", "typeName": "LinkedIn", "url": "https://d2ojpxxtu63wzl.cloudfront.net/static/d87d9548cca163016e66d9bd2021e009_603c580ad7e4028483f349edf054e54d389590d48330453a41f4011f498bf83d" }, { "type": "gravatar", "typeId": "gravatar", "typeName": "Gravatar", "url": "https://d2ojpxxtu63wzl.cloudfront.net/static/a3fbc2a91e003309dd5618b6ed7fea98_c594e9af3addf59ba41a907ba5784ba8ee23fb37089f1e1dbbba039d3cff05ff" }, { "type": "angellist", "typeId": "angellist", "typeName": "Facebook", "url": "https://d2ojpxxtu63wzl.cloudfront.net/static/dd7714430e06e43780b6b573c9805795_b1af65e8490498640027007c414caf88b4668d270fad927d9c74a798948c7bca" }], "contactInfo": { "websites": [{ "url": "http://teamtreehouse.com/kylepinzon" }], "familyName": "Pinzon", "fullName": "Kyle Pinzon", "givenName": "Kyle" }, "organizations": [{ "name": "Digsy", "startDate": "2012-12-31", "title": "Customer Success Manager", "current": true }, { "name": "BrokerRoster", "startDate": "2012-06-30", "title": "Community Manager and Growth Hacker", "current": true }, { "name": "Digsy", "startDate": "2012-06", "title": "Director of Client Success", "current": true }, { "name": "Integrated Resources Institute", "startDate": "2012-06", "endDate": "2013-08", "title": "Job Coach" }, { "name": "JP Morgan Chase Bank", "startDate": "2007-12", "endDate": "2012" }], "demographics": { "locationDeduced": { "normalizedLocation": "La Habra, California", "deducedLocation": "La Habra, California, United States", "city": { "name": "La Habra" }, "state": { "name": "California", "code": "CA" }, "country": { "deduced": true, "name": "United States", "code": "US" }, "continent": { "deduced": true, "name": "North America" }, "county": { "deduced": true, "name": "Orange" }, "likelihood": 1 }, "gender": "Male", "locationGeneral": "La Habra, California" }, "socialProfiles": [{ "followers": 9, "type": "angellist", "typeId": "angellist", "typeName": "AngelList", "url": "https://angel.co/kyle-pinzon", "username": "kyle-pinzon", "id": "803582" }, { "type": "facebook", "typeId": "facebook", "typeName": "Facebook", "url": "https://www.facebook.com/kyle.pinzon" }, { "type": "flickr", "typeId": "flickr", "typeName": "Flickr", "url": "https://www.flickr.com/people/69246681@N07", "username": "kylepinzon", "id": "69246681@N07" }, { "type": "foursquare", "typeId": "foursquare", "typeName": "Foursquare", "url": "https://foursquare.com/user/3073730", "id": "3073730" }, { "type": "github", "typeId": "github", "typeName": "Github", "url": "https://github.com/kpinzon", "username": "kpinzon" }, { "type": "google", "typeId": "google", "typeName": "GooglePlus", "url": "https://plus.google.com/107747333110667626493", "id": "107747333110667626493" }, { "type": "gravatar", "typeId": "gravatar", "typeName": "Gravatar", "url": "https://gravatar.com/kylepinzon", "username": "kylepinzon", "id": "49801145" }, { "followers": 212, "following": 212, "type": "linkedin", "typeId": "linkedin", "typeName": "LinkedIn", "url": "https://www.linkedin.com/in/kylepinzon", "username": "kylepinzon" }] };
-*/
+    $scope.hideFCEmailResults = function () {
+        $scope.fcByEmailShown = false;
+    }
+
 });
 
